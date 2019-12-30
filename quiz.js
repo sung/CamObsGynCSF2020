6a62c6d790202b6853207498531d56dc1cb26668
Sung Gong <sung@bio.cc>
2019-12-30 17:25:32 +0000

    stallAttend arg to quiz.js v0

--- quiz.js
+++ quiz.js
@@ -15,9 +15,9 @@ var quiz_count = 0;
 // or javascript object
 $.fn.quiz = function(filename, stallAttend) {
   if (typeof filename === "string") {
-    $.getJSON(filename, render.bind(this, stallAttend));
+    $.getJSON(filename, render.bind(this));
   } else {
-    render.call(this, filename, stallAttend);
+    render.call(this, filename);
   }
 };
 
